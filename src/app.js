65c9c87f931ee8962732e7fe3c39e7cd7548557c
Bethania Sanchez <bethasanchez3@gmail.com>
2021-12-22 09:34:53 -0400

    Starting to work on the task

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = () => {
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let number_index = Math.floor(Math.random() * number.length);
+    return number[number_index];
+  };
+  document.querySelector(".number").innerHTML = randomNumber();
+
+  let randomSuit = () => {
+    let suit_top = ["♦", "♥", "♠", "♣"];
+    let suit_bottom = ["♦", "♥", "♠", "♣"];
+    let suit_index = Math.floor(Math.random() * suit_top.length);
+    return suit_top[suit_index], suit_bottom[suit_index];
+  };
+  document.querySelector(".suit_top").innerHTML = randomSuit();
+  document.querySelector(".suit_bottom").innerHTML = randomSuit();
 };
